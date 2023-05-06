4c503b8399b1026b493722ea4e7333d6aef7013d
badaistracted <62783158+badaistracted@users.noreply.github.com>
2023-05-06 12:29:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="MichaelTownley";
-names[1]="JohnnyInMyHead";
-names[2]="JamesPumphrey";
-names[3]="JsonVorheez";
-names[4]="trevorphllips";
-names[5]="franklinclinton";
-names[6]="larrybird";
-names[7]="obama";
-names[8]="kachika";
-names[9]="jimhalpert";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
